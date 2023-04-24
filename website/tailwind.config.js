f1787e510459a8f68fe8319bd7b3c857d4c90e23
abodoma17 <o.a.ahmed3@student.aast.edu>
2023-04-24 21:40:00 +0200

    Initiated tailwind project, added landing page

---
+++ website/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        white: '#F4F3EE',
+        navy: '#02394A',
+        green: '#2DA95C',
+      },
+    },
+  },
+  plugins: [],
+}
+
