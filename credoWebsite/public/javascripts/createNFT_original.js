73d105dca50cdcde19ac9426d4686529d241f142
makarious <tmakariuos@gmail.com>
2023-07-06 20:25:31 +0300

    errorNULL

--- credoWebsite/public/javascripts/createNFT_original.js
+++ credoWebsite/public/javascripts/createNFT_original.js
@@ -20,7 +20,7 @@ let account;
 const accountEl = document.getElementById('account');
 document.getElementById("button1").onclick = createBat;
 
-async function addTokenToDB(batch_num, token_ID){
+async function addTokenToDB(batch_num, token_ID) {
     fetch("/batch/create", {
         method: "POST",
         body: JSON.stringify({
@@ -30,19 +30,16 @@ async function addTokenToDB(batch_num, token_ID){
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
-        })
+    })
         .then((response) => response.json())
         .then((json) => console.log(json));
 }
 
-async function createBat(){
+async function createBat() {
 
     await Moralis.start({
-            apiKey: "TEJaUyO5l5d9hmTfZ29HWw8WtEEZZeJZ3OLPwOli57z6447CA0nv0GUrXdTiBaB5",
+        apiKey: "TEJaUyO5l5d9hmTfZ29HWw8WtEEZZeJZ3OLPwOli57z6447CA0nv0GUrXdTiBaB5",
     });
-    
-
-
 
     let batch_num = document.getElementById("inputBatch").value;
     let owner_name = document.getElementById("manufacturerName").value;
@@ -51,45 +48,43 @@ async function createBat(){
     let production_date = document.getElementById("inputProduction").value;
     let token_creation_date = new Date().toJSON();
 
-    let json_file = JSON.stringify({
-        batch_num,
-        owner_name,
-        medicine_name,
-        expiration_date,
-        production_date,
-        token_creation_date
-    });
-    
-    const abi = [
-        {
-            path: 'file.json',
-            content: {
-                batch_num: batch_num,
-                owner_name: owner_name,
-                medicine_name: medicine_name,
-                expiration_date: expiration_date,
-                production_date: production_date,
-                token_creation_date: token_creation_date
+    if (batch_num === null || owner_name === null || medicine_name === null || expiration_date === null || production_date === null) {
+        document.getElementById('errorNULL').textContent = "Please fill in all the required fields.";
+    }
+    else {
+
+        const abi = [
+            {
+                path: 'file.json',
+                content: {
+                    batch_num: batch_num,
+                    owner_name: owner_name,
+                    medicine_name: medicine_name,
+                    expiration_date: expiration_date,
+                    production_date: production_date,
+                    token_creation_date: token_creation_date
+                }
             }
-        }
-    ];
+        ];
 
-    console.log(abi);
+        console.log(abi);
 
-    const response = await Moralis.EvmApi.ipfs.uploadFolder({ abi });
+        const response = await Moralis.EvmApi.ipfs.uploadFolder({ abi });
 
-    let responseJson = response.toJSON();
-    console.log(responseJson);
-    let path = responseJson[0].path;
-    console.log(path);
+        let responseJson = response.toJSON();
+        console.log(responseJson);
+        let path = responseJson[0].path;
+        console.log(path);
 
-    let new_id = await contract.methods.mintNFT(path).send({from:account});
-    console.log(new_id);
+        let new_id = await contract.methods.mintNFT(path).send({ from: account });
+        console.log(new_id);
 
-    let currentTokenCounter = await contract.methods.getLatestID().call();
-    console.log(currentTokenCounter);
+        let currentTokenCounter = await contract.methods.getLatestID().call();
+        console.log(currentTokenCounter);
 
-    addTokenToDB(batch_num, currentTokenCounter);
+        addTokenToDB(batch_num, currentTokenCounter);
+    }
+
 }
 
 const main = async () => {
